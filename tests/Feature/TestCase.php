ed0675cecb68b9476623557ebbb21ea6d791bac1
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2024-05-02 15:25:55 -0700

    GH-1

---
+++ tests/Feature/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Playground
+ */
+
+declare(strict_types=1);
+namespace Tests\Feature\Playground\Make\Controller;
+
+use Tests\Unit\Playground\Make\Controller\TestCase as BaseTestCase;
+
+/**
+ * \Tests\Feature\Playground\Make\Controller\TestCase
+ */
+class TestCase extends BaseTestCase
+{
+}
---
+++ tests/Unit/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Playground
+ */
+
+declare(strict_types=1);
+namespace Tests\Unit\Playground\Make\Controller;
+
+use Playground\Test\OrchestraTestCase;
+
+/**
+ * \Tests\Unit\Playground\Make\Controller\TestCase
+ */
+class TestCase extends OrchestraTestCase
+{
+    use FileTrait;
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Playground\ServiceProvider::class,
+            \Playground\Make\ServiceProvider::class,
+            \Playground\Make\Controller\ServiceProvider::class,
+        ];
+    }
+}
